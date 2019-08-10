b7423b4256e830a54d22af5a809c7bd5a036f5b2
Mak Fof <43025563+mf3129@users.noreply.github.com>
2019-08-10 10:32:05 -0400

    Week 5 Programming Work Update Question 1

---
+++ mf1836_hw5_q1.cpp
@@ -0,0 +1,48 @@
+//Write your; code here.
+////Make sure to upload your file.
+//Question 1:
+//Write a program that reads a positive integer n from the user and prints out a nxn multiplication table.
+//
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    //Practice 1
+    
+    int tableSize;
+    int count, firstRowValue;
+    
+    cout << "Enter a number to use: " << endl;
+    cin >> tableSize;
+    
+    firstRowValue = 1;
+    
+    //Iterate and create the rows
+    for (int row = 1; row <= tableSize; row++ ){
+        //Create Columns
+        for (int column = 1; column <= tableSize; column++) {
+            //Multiply each number in column by the firstRowValue
+            cout << firstRowValue * column;
+            if ((firstRowValue * column) >= 10) {
+                cout << "  ";
+            } else {
+                cout << "   ";
+            }
+        }
+    
+        cout << endl;
+        //Increment firstRowValue
+        firstRowValue++;
+    
+    
+    }
+    
+    
+        return 0;
+    }
+    
+
+
