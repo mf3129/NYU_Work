71ef51a90a614a0c037ac18f2b8e6cb0aa51ab81
Mak Fof <43025563+mf3129@users.noreply.github.com>
2019-08-25 20:05:08 -0400

    Week 7 Programming Work Update Question 1
    Part A answered

---
+++ mf1836_hw7_q1.cpp
@@ -0,0 +1,70 @@
+/*
+ 
+ Implement a function:
+ int printMonthCalender(int numOfDays, int startingDay)
+ This function is given two parameters:
+ numOfDays - The number of days in the month
+ startingDay – a number 1-7 that represents the day in the week of the first day
+ in that month (1 for Monday, 2 for Tuesday, 3 for Wednesday, etc.).
+ 
+ 
+ 
+ */
+
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+
+
+//Declaring Function Prototype
+int printMonthCalendar(int numOfDays, int startingDay);
+
+
+
+
+int main() {
+    
+    printMonthCalendar(31, 4);
+    
+    return 0;
+}
+
+
+//Defining Functions
+int printMonthCalendar(int numOfDays, int startingDay) {
+    
+    //Printing the header
+    cout << "Mon" << '\t' << "Tue" << '\t' << "Wed" << '\t' << "Thr" << '\t' << "Fri" << '\t' << "Sat" << '\t' << "Sun" << endl;
+    
+        int currentDay = 0, finalDay, positionNumber = 1;
+    
+        for (positionNumber; positionNumber < numOfDays + startingDay; positionNumber++) {
+            
+            if (positionNumber < startingDay) {
+                cout << "\t";
+            }
+            
+            if (positionNumber >= startingDay && currentDay <= numOfDays) {
+                
+                currentDay++;
+                cout << currentDay << "\t";
+                
+                if (positionNumber % 7 == 0) {
+                    cout << endl;
+                }
+                
+            }
+        
+        }
+    
+    finalDay = (positionNumber - 1) % 7;
+    
+    cout << "\nThe day number in the week of the last day in the month is: " << finalDay << endl;
+    
+    return finalDay;
+}
